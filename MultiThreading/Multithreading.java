a36c3ff12ea99ee6df7f46f36d2978d24d271b81
manohar sai <powermart@gmail.com>
2024-11-09 23:28:08 +0530

    core java

---
+++ MultiThreading/Multithreading.java
@@ -0,0 +1,40 @@
+package MultiThreading;
+
+public class Multithreading {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stu
+		
+		/* Using implementation
+		 * Employee e1 = new  Employee();
+		 * Thread t1 = new Thread(e1);
+		 */
+		Employee e1 = new Employee();
+		Manager m1 = new Manager();
+		
+		e1.start();
+		m1.start();
+		//Lambda exp
+		Runnable r1 = () -> System.out.println("user"+Thread.currentThread().toString());
+		Thread t1 = new Thread(r1);
+		t1.start();
+		
+		
+
+	}
+
+}
+
+class Employee extends Thread implements Runnable
+{
+	public void run() {
+		System.out.println("Emp"+Thread.currentThread().toString());
+	}
+}
+
+class Manager extends Thread
+{
+	public void run() {
+		System.out.println("Manager"+Thread.currentThread().toString());
+	}
+}
